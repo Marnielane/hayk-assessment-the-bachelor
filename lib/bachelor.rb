1a09a13a608e2f06b27418131398f321991f08e0
Marnie Robinson <marnielane@gmail.com>
2021-02-03 16:34:30 -0700

    commit tree d53daf46a72b0160372afb4fb7f532a8f118abea

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,50 @@
+require 'pry'
+require 'json'
+file = File.read('./spec/fixtures/contestants.json')
+contestants = JSON.parse(file)
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = data[season].find { | contestant | contestant["status"] == "Winner" }
+      winner["name"].split[0]
+end
+
+def all_contestant_data(data)
+  data.values.flatten
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = nil
+  contestants = all_contestant_data(data)
+  contestants.find do |attributes|
+    if attributes["occupation"] == occupation
+      name = attributes["name"]
+    end
+  end
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  contestants = all_contestant_data(data)
+  contestants.count do |contestant|
+      contestant["hometown"] == hometown
+  end
 end
 
 def get_occupation(data, hometown)
-  # code here
+  occupation = nil
+  contestants = all_contestant_data(data)
+  contestants.find do |attributes|
+    if attributes["hometown"] == hometown
+      occupation = attributes["occupation"]
+    end
+  end
+  occupation
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total_age = data[season].sum { |contestant|
+    contestant["age"].to_f }
+  total_contestants = data[season].length
+  (total_age.to_f / total_contestants.to_f).round
 end
+
